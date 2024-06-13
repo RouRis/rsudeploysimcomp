943a5d2d34a7a61937067904e0d7b89d495a38f9
Rouven Rischert <rischertrouven@gmail.com>
2024-06-13 15:55:35 +0200

    Matrix M & P generation

--- rsudeploysimcomp/sumo_interface/xml_parser.py
+++ rsudeploysimcomp/sumo_interface/xml_parser.py
@@ -1,6 +1,6 @@
 import gzip
 import xml.etree.ElementTree as ET
-
+from scipy.sparse import lil_matrix, csr_matrix
 import numpy as np
 
 from rsudeploysimcomp.utils.config_loader import load_config
@@ -62,10 +62,11 @@ def parse_max_xy():
 class PmcpBParser:
     def __init__(self):
         self.grid_size = int(config["sumo_interface"]["xml_parser"]["grid_size"])  # Number of cells per dimension
-        self.vehicle_count = np.zeros((self.grid_size, self.grid_size), dtype=int)
+        self.M = np.zeros((self.grid_size, self.grid_size), dtype=int)
+        self.P = lil_matrix((self.grid_size * self.grid_size, self.grid_size * self.grid_size), dtype=float)  # Sparse migration matrix
         self.x_max, self.y_max = parse_max_xy()
         self.x_min, self.y_min = 0, 0
-        self.generate_matrix_m()
+        self.generate_matrix_m_and_p()
 
     def get_grid_cell(self, x, y):
         """
@@ -88,30 +89,62 @@ class PmcpBParser:
 
         return x_index, y_index
 
-    def generate_matrix_m(self):
+    def generate_matrix_m_and_p(self):
         """
-        Generates a 2D matrix of vehicle counts within a grid based on an XML file.
+        Generates a 2D matrix of vehicle counts within a grid (matrix M)
+        and a matrix of migration ratios between adjacent grid cells (matrix P).
 
         Returns:
-            numpy.ndarray: 2D matrix representing vehicle counts in each grid cell.
+            numpy.ndarray: 2D matrix M, representing vehicle counts in each grid cell.
+            numpy.ndarray: 2D matrix P, representing migration ratios between adjacent grid cells.
         """
+        vehicle_locations = {}
+        vehicle_ids = [[set() for _ in range(self.grid_size)] for _ in range(self.grid_size)]
         try:
             # Parse the XML file
             tree = ET.parse(path_to_fcd_xml)
             root = tree.getroot()
+
             # Iterate over each timestep
             for timestep in root.findall("timestep"):
                 # Iterate over each vehicle within the timestep
                 for vehicle in timestep.findall("vehicle"):
+                    vehicle_id = vehicle.get("id")
                     x = float(vehicle.get("x"))
                     y = float(vehicle.get("y"))
-                    x_index, y_index = self.get_grid_cell(x, y)
-                    self.vehicle_count[y_index, x_index] += 1
+                    current_cell = self.get_grid_cell(x, y)
+                    vehicle_ids[current_cell[0]][current_cell[1]].add(vehicle_id)
+
+                    # Update the vehicle's current location
+                    if vehicle_id not in vehicle_locations:
+                        vehicle_locations[vehicle_id] = {'previous': None, 'current': current_cell}
+                    else:
+                        vehicle_locations[vehicle_id]['previous'] = vehicle_locations[vehicle_id]['current']
+                        vehicle_locations[vehicle_id]['current'] = current_cell
+
+                    # If the vehicle has a previous location, update matrix P
+                    previous_cell = vehicle_locations[vehicle_id]['previous']
+                    if previous_cell is not None and previous_cell != current_cell:
+                        from_index = previous_cell[0] * self.grid_size + previous_cell[1]
+                        to_index = current_cell[0] * self.grid_size + current_cell[1]
+                        self.P[from_index, to_index] += 1
+
+            # Count the number of unique vehicles for each grid cell
+            for i in range(self.grid_size):
+                for j in range(self.grid_size):
+                    self.M[i, j] = len(vehicle_ids[i][j])
+
+            # Normalize matrix P to get migration ratios
+            for i in range(self.P.shape[0]):
+                row_sum = np.sum(self.P[i])
+                if row_sum > 0:
+                    self.P[i] /= row_sum
 
         except Exception as e:
             print(f"An error occurred while processing the XML file: {e}")
+        self.P = self.P.tocsr()
 
 
 pmcp_b_parser = PmcpBParser()
-print("2")
-print(pmcp_b_parser.vehicle_count)
+print(pmcp_b_parser.M)
+print(pmcp_b_parser.P)
